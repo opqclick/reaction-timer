93f57c6147da557043a6f95e2ca682a945f41b3a
Opu Hasnat <opqclick@gmail.com>
2022-06-18 01:57:56 +0600

    Final commit

---
+++ src/components/Results.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>Reaction time: {{ score }}ms</p>
+  <h2>{{ rank }}</h2>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail pace";
+    }
+  },
+};
+</script>
+
+<style></style>
